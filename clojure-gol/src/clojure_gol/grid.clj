1bb393965d38b6e84782acdf24e0e191c7890b02
Jon Eland <jonathan.eland@gmail.com>
2013-10-11 17:41:14 +0100

    Added ability to update specific index to live in grid

--- clojure-gol/src/clojure_gol/grid.clj
+++ clojure-gol/src/clojure_gol/grid.clj
@@ -2,3 +2,6 @@
 
 (defn generate-grid [size]
   (repeat size (vec (repeat size :dead))))
+
+(defn set-index-to-live [grid row column]
+  (assoc grid row (assoc (grid row) column :live)))
--- clojure-gol/test/clojure_gol/grid_test.clj
+++ clojure-gol/test/clojure_gol/grid_test.clj
@@ -6,3 +6,8 @@
   (testing "grid is created with given size"
     (is (= (generate-grid 1) [[:dead]]))
     (is (= (generate-grid 2) [[:dead :dead] [:dead :dead]]))))
+
+(deftest set-given-index-to-live 
+  (testing "grid is updated with :live at given index"
+    (is (= (set-index-to-live [[:dead]] 0 0) [[:live]]))
+    (is (= (set-index-to-live [[:dead :dead][:dead :dead]] 0 1) [[:dead :live][:dead :dead]]))))
