63c12cc0aa32cf8fae808485d50696739854f63a
Jon Eland <jonathan.eland@gmail.com>
2013-10-11 18:28:32 +0100

    First attempt at updating multiple indexes in the grid

--- clojure-gol/src/clojure_gol/grid.clj
+++ clojure-gol/src/clojure_gol/grid.clj
@@ -5,3 +5,11 @@
 
 (defn set-index-to-live [grid row column]
   (assoc grid row (assoc (grid row) column :live)))
+
+(defn evolve-grid [grid live-cells]
+  (loop [cells live-cells
+         evolved-grid grid]
+    (if (empty? cells)
+      evolved-grid
+      (recur (rest cells)
+             (set-index-to-live evolved-grid ((first cells) :row) ((first cells) :column))))))
--- clojure-gol/test/clojure_gol/grid_test.clj
+++ clojure-gol/test/clojure_gol/grid_test.clj
@@ -11,3 +11,7 @@
   (testing "grid is updated with :live at given index"
     (is (= (set-index-to-live [[:dead]] 0 0) [[:live]]))
     (is (= (set-index-to-live [[:dead :dead][:dead :dead]] 0 1) [[:dead :live][:dead :dead]]))))
+
+(deftest evolve-grid-to-next-representation
+  (testing "representation of grid after evolve"
+    (is (= (evolve-grid [[:dead :dead][:dead :dead]] [{:row 0 :column 0} {:row 1 :column 1}]) [[:live :dead][:dead :live]]))))
