f703c6586f01c8e700421928092ae8a68a843472
Jon Eland <jonathan.eland@gmail.com>
2013-10-16 13:26:02 +0100

    Formating evolve-grid function

--- clojure-gol/src/clojure_gol/grid.clj
+++ clojure-gol/src/clojure_gol/grid.clj
@@ -4,4 +4,7 @@
   (repeat size (vec (repeat size :dead))))
 
 (defn evolve-grid [live-cells]
-  (map (fn [row rows-live-cells] (reduce #(assoc %1 %2 :live) row rows-live-cells)) (generate-grid (count live-cells)) live-cells))
+  (map 
+    (fn [row rows-live-cells] (reduce #(assoc %1 %2 :live) row rows-live-cells))
+    (generate-grid (count live-cells))
+    live-cells))
