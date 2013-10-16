f87c1a0764acde89e26d297154160af2fed2f338
Jon Eland <jonathan.eland@gmail.com>
2013-10-16 14:50:01 +0100

    Cleaned up parameter name

--- clojure-gol/src/clojure_gol/grid.clj
+++ clojure-gol/src/clojure_gol/grid.clj
@@ -6,6 +6,6 @@
 (defn set-live-cells [row cells]
   (reduce #(assoc %1 %2 :live) row cells))
 
-(defn evolve-grid [index-of-live-cells]
-  (let [dead-grid (generate-grid (count index-of-live-cells))]
-    (map set-live-cells dead-grid index-of-live-cells)))
+(defn evolve-grid [indexes-of-live-cells]
+  (let [dead-grid (generate-grid (count indexes-of-live-cells))]
+    (map set-live-cells dead-grid indexes-of-live-cells)))
